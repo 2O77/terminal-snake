8eacd4724f3587d7ea4cba2f4cf0eb1743efae69
2O77 <recepahmetkara@gmail.com>
2025-10-18 16:48:31 +0300

    feat(moevement): added kinda complesive move pattern. rn it moves like real snake game.

--- main.go
+++ main.go
@@ -13,66 +13,58 @@ type Snake struct {
 	RowEnd   int
 }
 
-func renderSnake(screen tcell.Screen, style tcell.Style, snake *Snake) {
-	for row := snake.RowStart; row <= snake.RowEnd; row++ {
-		for col := snake.ColStart; col <= snake.ColEnd; col++ {
-			screen.SetContent(col, row, ' ', nil, style)
-		}
-	}
+// 2 column wide 5 row long
+type SnakePiece struct {
+	RowEnd int
+	ColEnd int
 }
 
-func moveSnakeUp(screen tcell.Screen, style tcell.Style, snake *Snake) {
+func moveSnakeUp(screen tcell.Screen, headStyle tcell.Style, bodyStyle tcell.Style, snake *[]SnakePiece) {
 	resetScreen(screen)
-	snake.RowStart -= 2
-	snake.RowEnd -= 2
-
-	for row := snake.RowStart; row <= snake.RowEnd; row++ {
-		for col := snake.ColStart; col <= snake.ColEnd; col++ {
-			screen.SetContent(col, row, ' ', nil, style)
+	if (*snake)[0].ColEnd <= (*snake)[1].ColEnd {
+		for i := len(*snake) - 1; i > 0; i-- {
+			(*snake)[i].ColEnd = (*snake)[i-1].ColEnd
+			(*snake)[i].RowEnd = (*snake)[i-1].RowEnd
 		}
+		(*snake)[0].ColEnd -= 2
 	}
+	renderSnake(screen, headStyle, bodyStyle, snake)
 }
 
-func moveSnakeDown(screen tcell.Screen, style tcell.Style, snake *Snake) {
+func moveSnakeDown(screen tcell.Screen, headStyle tcell.Style, bodyStyle tcell.Style, snake *[]SnakePiece) {
 	resetScreen(screen)
-	snake.RowStart += 2
-	snake.RowEnd += 2
-
-	for row := snake.RowStart; row <= snake.RowEnd; row++ {
-		for col := snake.ColStart; col <= snake.ColEnd; col++ {
-			screen.SetContent(col, row, ' ', nil, style)
+	if (*snake)[0].ColEnd >= (*snake)[1].ColEnd {
+		for i := len(*snake) - 1; i > 0; i-- {
+			(*snake)[i].ColEnd = (*snake)[i-1].ColEnd
+			(*snake)[i].RowEnd = (*snake)[i-1].RowEnd
 		}
+		(*snake)[0].ColEnd += 2
 	}
+	renderSnake(screen, headStyle, bodyStyle, snake)
 }
 
-func moveSnakeLeft(screen tcell.Screen, style tcell.Style, snake *Snake) {
+func moveSnakeLeft(screen tcell.Screen, headStyle tcell.Style, bodyStyle tcell.Style, snake *[]SnakePiece) {
 	resetScreen(screen)
-
-	snake.ColStart -= 5
-	snake.ColEnd -= 5
-
-	for row := snake.RowStart; row <= snake.RowEnd; row++ {
-		for col := snake.ColStart; col <= snake.ColEnd; col++ {
-			screen.SetContent(col, row, ' ', nil, style)
+	if (*snake)[0].RowEnd <= (*snake)[1].RowEnd {
+		for i := len(*snake) - 1; i > 0; i-- {
+			(*snake)[i].ColEnd = (*snake)[i-1].ColEnd
+			(*snake)[i].RowEnd = (*snake)[i-1].RowEnd
 		}
+		(*snake)[0].RowEnd -= 5
 	}
+	renderSnake(screen, headStyle, bodyStyle, snake)
 }
 
-func moveSnakeRight(screen tcell.Screen, style tcell.Style, snake *Snake) {
+func moveSnakeRight(screen tcell.Screen, headStyle tcell.Style, bodyStyle tcell.Style, snake *[]SnakePiece) {
 	resetScreen(screen)
-
-	snake.ColStart += 5
-	snake.ColEnd += 5
-
-	for row := snake.RowStart; row <= snake.RowEnd; row++ {
-		for col := snake.ColStart; col <= snake.ColEnd; col++ {
-			screen.SetContent(col, row, ' ', nil, style)
+	if (*snake)[0].RowEnd >= (*snake)[1].RowEnd {
+		for i := len(*snake) - 1; i > 0; i-- {
+			(*snake)[i].ColEnd = (*snake)[i-1].ColEnd
+			(*snake)[i].RowEnd = (*snake)[i-1].RowEnd
 		}
+		(*snake)[0].RowEnd += 5
 	}
-}
-
-func getSnakeHead(snake *Snake) {
-
+	renderSnake(screen, headStyle, bodyStyle, snake)
 }
 
 func resetScreen(screen tcell.Screen) {
@@ -83,12 +75,33 @@ func resetScreen(screen tcell.Screen) {
 			screen.SetContent(col, row, ' ', nil, boxStyle)
 		}
 	}
+
+}
+
+func renderSnake(screen tcell.Screen, headStyle tcell.Style, bodyStyle tcell.Style, snake *[]SnakePiece) {
+	for i := 0; i < len(*snake); i++ {
+		p := (*snake)[i]
+
+		if i == 0 {
+			for row := p.ColEnd - 1; row <= p.ColEnd; row++ {
+				for col := p.RowEnd - 4; col <= p.RowEnd; col++ {
+					screen.SetContent(col, row, ' ', nil, headStyle)
+				}
+			}
+		} else {
+			for row := p.ColEnd - 1; row <= p.ColEnd; row++ {
+				for col := p.RowEnd - 4; col <= p.RowEnd; col++ {
+					screen.SetContent(col, row, ' ', nil, bodyStyle)
+				}
+			}
+		}
+	}
 }
 
 func main() {
 	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
-	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.Color100)
-	snakeStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorBlack)
+	snakeBodyStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorBlack)
+	snakeHeadStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorDarkGray)
 	appleStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorRed)
 
 	s, err := tcell.NewScreen()
@@ -103,23 +116,15 @@ func main() {
 	s.EnablePaste()
 	s.Clear()
 
-	snake := &Snake{
-		ColStart: 10,
-		ColEnd:   25,
-		RowStart: 5,
-		RowEnd:   6,
+	pieces := []SnakePiece{
+		{29, 6}, {24, 6}, {19, 6}, {14, 6},
 	}
 
-	for row := 5; row <= 40; row++ {
-		for col := 10; col <= 200; col++ {
-			s.SetContent(col, row, ' ', nil, boxStyle)
-		}
-	}
+	renderSnake(s, snakeHeadStyle, snakeBodyStyle, &pieces)
+	resetScreen(s)
 
-	renderSnake(s, snakeStyle, snake)
-
-	for row := 10; row <= 11; row++ {
-		for col := 16; col <= 20; col++ {
+	for row := 11; row <= 12; row++ {
+		for col := 15; col <= 19; col++ {
 			s.SetContent(col, row, ' ', nil, appleStyle)
 		}
 	}
@@ -146,16 +151,16 @@ func main() {
 			}
 
 			if ev.Key() == tcell.KeyUp {
-				moveSnakeUp(s, snakeStyle, snake)
+				moveSnakeUp(s, snakeHeadStyle, snakeBodyStyle, &pieces)
 			}
 			if ev.Key() == tcell.KeyDown {
-				moveSnakeDown(s, snakeStyle, snake)
+				moveSnakeDown(s, snakeHeadStyle, snakeBodyStyle, &pieces)
 			}
 			if ev.Key() == tcell.KeyLeft {
-				moveSnakeLeft(s, snakeStyle, snake)
+				moveSnakeLeft(s, snakeHeadStyle, snakeBodyStyle, &pieces)
 			}
 			if ev.Key() == tcell.KeyRight {
-				moveSnakeRight(s, snakeStyle, snake)
+				moveSnakeRight(s, snakeHeadStyle, snakeBodyStyle, &pieces)
 			}
 
 		}
