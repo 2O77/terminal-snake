ecfdbda986a603986b388e7629dc67f9c256b7b6
2O77 <recepahmetkara@gmail.com>
2026-08-26 22:31:05 +0300

    working on subticks but not yet finished. but moves are calculated correctly
    first commit

---
+++ timer/timer.go
@@ -0,0 +1,26 @@
+package timer
+
+import "time"
+
+func NewTime(size int) time.Duration {
+	if size < 15 {
+		return 180
+	}
+	if size < 25 {
+		return 160
+	}
+	if size < 35 {
+		return 140
+	}
+	if size < 45 {
+		return 130
+	}
+	if size < 55 {
+		return 110
+	}
+	if size < 65 {
+		return 100
+	}
+
+	return 180
+}
